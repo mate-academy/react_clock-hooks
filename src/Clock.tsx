9556775da3769dbdb4fe44436a7f59b4da02ffc1
Ilija Pejanovic <ikadvm96@gmail.com>
2023-01-19 18:35:02 +0100

    solution

---
+++ src/Clock.tsx
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from 'react';
+
+type Props = {
+  clockName: string;
+};
+
+export const Clock:React.FC<Props> = ({ clockName }) => {
+  const [currentTime, setCurrentTime] = useState(new Date());
+  const [previousClockName, setPreviousClockName] = useState(clockName);
+
+  useEffect(() => {
+    const timerId = window.setInterval(() => {
+      setCurrentTime(new Date());
+      // eslint-disable-next-line no-console
+      console.info(new Date().toUTCString().slice(-12, -4));
+    }, 1000);
+
+    return () => {
+      window.clearInterval(timerId);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (previousClockName !== clockName) {
+      // eslint-disable-next-line no-console
+      console.debug(`Renamed to ${clockName}`);
+      setPreviousClockName(clockName);
+    }
+  }, [clockName]);
+
+  return (
+    <div className="Clock">
+      <strong className="Clock__name">
+        {clockName}
+      </strong>
+
+      {' time is '}
+
+      <span className="Clock__time">
+        {currentTime.toUTCString().slice(-12, -4)}
+      </span>
+    </div>
+  );
+};
