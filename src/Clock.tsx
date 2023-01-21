f0b6c287ba7c14c96cc6af56af42cf588443af3a
Lefteris Loizides <loizides.lef@gmail.com>
2023-01-21 09:56:17 +0200

    fix test

---
+++ src/Clock.tsx
@@ -0,0 +1,31 @@
+/* eslint-disable no-console */
+import { useEffect, useState } from 'react';
+
+export const Clock: React.FC<{ name: string }> = ({ name }) => {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const timerId = window.setInterval(() => {
+      const newTime = new Date();
+
+      setTime(newTime);
+      console.info(newTime.toUTCString().slice(-12, -4));
+    }, 1000);
+
+    return () => window.clearInterval(timerId);
+  });
+
+  return (
+    <div className="Clock">
+      <strong className="Clock__name">
+        {name}
+      </strong>
+
+      {' time is '}
+
+      <span className="Clock__time">
+        {time.toUTCString().slice(-12, -4)}
+      </span>
+    </div>
+  );
+};
