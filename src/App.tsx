3f075bedf97f0a89e06681a4f91874bccfbc97dc
Ala El Achkar <iamalaaashkar97@gmail.com>
2023-01-22 21:05:49 +0300

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import './App.scss';
+import { Clock } from './clock';
 
 function getRandomName(): string {
   const value = Date.now().toString().slice(-4);
@@ -8,63 +9,41 @@ function getRandomName(): string {
 }
 
 export const App: React.FC = () => {
-  const today = new Date();
   const [clockName, setClockName] = useState('Clock-0');
-  const [time, setTime] = useState(today);
-  const [showClock, setShowClock] = useState(true);
+  const [visibleclock, setVisibleClock] = useState(true);
 
-  const timerId = window.setInterval(() => {
-    setClockName(getRandomName);
-  }, 3300);
+  const handleRightClick = (event: MouseEvent) => {
+    event.preventDefault();
+    setVisibleClock(false);
+  };
 
-  window.clearInterval(timerId);
+  const handleLeftClick = () => {
+    setVisibleClock(true);
+  };
 
   useEffect(() => {
-    window.setInterval(() => {
-      setClockName(getRandomName);
+    document.addEventListener('click', handleLeftClick);
+    document.addEventListener('contextmenu', handleRightClick);
+
+    const timerId = window.setInterval(() => {
+      setClockName(getRandomName());
     }, 3300);
 
     return () => {
+      document.removeEventListener('click', handleLeftClick);
+      document.removeEventListener('contextmenu', handleRightClick);
+
       window.clearInterval(timerId);
     };
   }, []);
 
-  useEffect(() => {
-    window.setInterval(() => {
-      setTime(new Date());
-      const newTime = new Date();
-
-      // eslint-disable-next-line no-console
-      console.info(newTime.toUTCString().slice(-12, -4));
-    }, 1000);
-  }, []);
-  document.addEventListener('contextmenu', (event) => {
-    event.preventDefault(); // not to show the context menu
-
-    setShowClock(false);
-  });
-
-  document.addEventListener('click', () => {
-    setShowClock(true);
-  });
-
   return (
     <div className="App">
       <h1>React clock</h1>
-      {showClock && (
-        <div className="Clock">
-          <strong className="Clock__name">
-            {clockName}
-          </strong>
-
-          {' time is '}
-
-          <span className="Clock__time">
-            {time.toUTCString().slice(-12, -4)}
-          </span>
-        </div>
-
+      {visibleclock && (
+        <Clock clockName={clockName} />
       )}
+
     </div>
   );
 };
